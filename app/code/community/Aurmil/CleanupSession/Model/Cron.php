b07319bad03178d4d77c3ac4866f68933965489f
Aurélien Millet <aurmil@users.noreply.github.com>
2014-12-16 10:02:17 +0100

    Cron.php were ignored

---
+++ app/code/community/Aurmil/CleanupSession/Model/Cron.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * @author     Aurélien Millet
+ * @link       https://github.com/aurmil/
+ */
+
+class Aurmil_CleanupSession_Model_Cron
+extends Mage_Core_Model_Abstract
+{
+    const XML_PATH_EMAIL_SESSION_CLEAN_TEMPLATE     = 'system/session_clean/error_email_template';
+    const XML_PATH_EMAIL_SESSION_CLEAN_IDENTITY     = 'system/session_clean/error_email_identity';
+    const XML_PATH_EMAIL_SESSION_CLEAN_RECIPIENT    = 'system/session_clean/error_email';
+    const XML_PATH_SESSION_CLEAN_ENABLED            = 'system/session_clean/enabled';
+
+    public function sessionClean()
+    {
+        if (Mage::getStoreConfigFlag(self::XML_PATH_SESSION_CLEAN_ENABLED)) {
+            $errorMessage = null;
+
+            switch (Mage::getSingleton('core/session')->getSessionSaveMethod()) {
+                case 'files':
+                    $sessions = glob(Mage::getSingleton('core/session')->getSessionSavePath()
+                                    . DS
+                                    . 'sess_*');
+
+                    if (is_array($sessions) && !empty($sessions)) {
+                        $timeLimit = time() - (int) Mage::getResourceSingleton('core/session')->getLifeTime();
+
+                        foreach ($sessions as $session) {
+                            if (file_exists($session) && is_file($session)
+                                && filemtime($session) < $timeLimit
+                            ) {
+                                if (!@unlink($session) && !$errorMessage) {
+                                    $errorMessage = Mage::helper('aurmil_cleanupsession')->__(
+                                        'Some sessions files could not be deleted.'
+                                    );
+                                }
+                            }
+                        }
+                    }
+
+                    break;
+
+                case 'db':
+                    try {
+                        // Mage::getSingleton('core/resource')->gc() is too random
+                        $resource = Mage::getSingleton('core/resource');
+                        $resource->getConnection('core_write')
+                            ->delete(
+                                $resource->getTableName('core/session'),
+                                array('session_expires < ?' => time(),
+                            ));
+                    } catch (Exception $e) {
+                        $errorMessage = Mage::helper('aurmil_cleanupsession')->__(
+                            'An error occurred while cleaning database session table: %s',
+                            "\n\n" . $e->getMessage() . "\n\n" . $e->getTraceAsString()
+                        );
+                    }
+
+                    break;
+
+                default:
+                    $errorMessage = Mage::helper('aurmil_cleanupsession')->__(
+                        'Unsupported session save method: %s',
+                        (string) Mage::getSingleton('core/session')->getSessionSaveMethod()
+                    );
+
+                    break;
+            }
+
+            if ($errorMessage
+                && Mage::getStoreConfig(self::XML_PATH_EMAIL_SESSION_CLEAN_RECIPIENT)
+            ) {
+                $emailTemplate = Mage::getModel('core/email_template');
+                /* @var $emailTemplate Mage_Core_Model_Email_Template */
+                $emailTemplate->setDesignConfig(array('area' => 'backend'))
+                    ->sendTransactional(
+                        Mage::getStoreConfig(self::XML_PATH_EMAIL_SESSION_CLEAN_TEMPLATE),
+                        Mage::getStoreConfig(self::XML_PATH_EMAIL_SESSION_CLEAN_IDENTITY),
+                        Mage::getStoreConfig(self::XML_PATH_EMAIL_SESSION_CLEAN_RECIPIENT),
+                        null,
+                        array('warnings' => $errorMessage)
+                    );
+            }
+        }
+    }
+}
---
+++ app/code/community/Aurmil/CleanupSession/Model/System/Config/Backend/Cron.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @author     Aurélien Millet
+ * @link       https://github.com/aurmil/
+ */
+
+class Aurmil_CleanupSession_Model_System_Config_Backend_Cron
+extends Mage_Core_Model_Config_Data
+{
+    const CRON_STRING_PATH  = 'crontab/jobs/session_clean/schedule/cron_expr';
+
+    protected function _afterSave()
+    {
+        $enabled   = $this->getData('groups/session_clean/fields/enabled/value');
+        $time      = $this->getData('groups/session_clean/fields/time/value');
+        $frequency = $this->getData('groups/session_clean/fields/frequency/value');
+
+        $frequencyDaily   = Mage_Adminhtml_Model_System_Config_Source_Cron_Frequency::CRON_DAILY;
+        $frequencyWeekly  = Mage_Adminhtml_Model_System_Config_Source_Cron_Frequency::CRON_WEEKLY;
+        $frequencyMonthly = Mage_Adminhtml_Model_System_Config_Source_Cron_Frequency::CRON_MONTHLY;
+
+        $cronExprString = '';
+        if ($enabled) {
+            $cronExprArray = array(
+                intval($time[1]),                              # Minute
+                intval($time[0]),                              # Hour
+                ($frequency == $frequencyMonthly) ? '1' : '*', # Day of the Month
+                '*',                                           # Month of the Year
+                ($frequency == $frequencyWeekly) ? '1' : '*',  # Day of the Week
+            );
+            $cronExprString = join(' ', $cronExprArray);
+        }
+
+        try {
+            Mage::getModel('core/config_data')
+                ->load(self::CRON_STRING_PATH, 'path')
+                ->setValue($cronExprString)
+                ->setPath(self::CRON_STRING_PATH)
+                ->save();
+        } catch (Exception $e) {
+            Mage::throwException(Mage::helper('adminhtml')->__('Unable to save the cron expression.'));
+        }
+    }
+}
